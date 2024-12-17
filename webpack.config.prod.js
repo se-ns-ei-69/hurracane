50056f9bace7069baa7518f9e8ea0fda28dc736c
Roman <romalasolution@icloud.com>
2024-12-17 07:47:31 +0200

    fix locales

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -12,6 +12,7 @@ module.exports = merge(common, {
     new CopyPlugin({
       patterns: [
         { from: 'img', to: 'img' },
+        { from: 'locales', to: 'locales' },
         { from: 'css', to: 'css' },
         { from: 'js/vendor', to: 'js/vendor' },
         { from: 'icon.svg', to: 'icon.svg' },
