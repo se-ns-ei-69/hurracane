5d28628a227c1a590f391d33256c916d992f4a02
Roman <romalasolution@icloud.com>
2024-12-21 20:21:37 +0200

    fix files

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -15,6 +15,7 @@ module.exports = merge(common, {
         { from: 'favicon.ico', to: 'favicon.ico' },
         { from: 'robots.txt', to: 'robots.txt' },
         { from: 'icon.png', to: 'icon.png' },
+        { from: 'index.html', to: 'index.html' },
         { from: '404.html', to: '404.html' },
         { from: 'site.webmanifest', to: 'site.webmanifest' },
       ],
