3eb1e05d5431c4293764c3b066a51b47a37e5694
Roman <romalasolution@icloud.com>
2024-12-21 20:17:01 +0200

    fix link

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,14 +1,10 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
     new CopyPlugin({
       patterns: [
         { from: 'img', to: 'img' },
